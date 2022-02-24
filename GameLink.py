d0df5989032a9e270f7c23808850f0420073a46c
ryjay84 <gallenberg9081@uwlax.edu>
2022-02-24 01:14:04 -0600

    Testing of parsing through JSON received through API call

--- GameLink.py
+++ GameLink.py
@@ -1,5 +1,6 @@
 # May have to install this package before using
 import requests
+import json
 
 
 def connect():
@@ -10,11 +11,18 @@ def connect():
     # This is what we will be using to gather info from the api, what comes after the v2/ is where you put the
     # command that you want to get (friends, account, group, etc.) from this site: https://xbl.io/console
     response = requests.get('https://xbl.io/api/v2/friends', headers=headers)
-    print(response.status_code)
-    print(response.json())
 
     # Use this to print header info if needed
     # print(response.headers)
+
+    print(response.status_code)
+    print(response.json())
+
+    # Parsing out from friends, who is a favorite and online (TEST)
+    resp_data = response.json()
+    for person in resp_data['people']:
+        if person['isFavorite'] and person['presenceState'] == 'Online':
+            print(person['displayName'])
 
 
 if __name__ == '__main__':
