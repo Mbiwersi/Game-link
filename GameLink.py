c87bd0792aa21870417c4efd1f968dfe0dc6773a
Ben Stenberg <bennystenny12@gmail.com>
2022-02-24 18:50:42 -0600

    pretty printing

--- GameLink.py
+++ GameLink.py
@@ -2,7 +2,6 @@
 import requests
 import json
 
-
 def connect():
     # Need this header to tie into my account info. If you want to test with your account, replace the
     # key from the OpenXBL Site
@@ -10,19 +9,38 @@ def connect():
 
     # This is what we will be using to gather info from the api, what comes after the v2/ is where you put the
     # command that you want to get (friends, account, group, etc.) from this site: https://xbl.io/console
-    response = requests.get('https://xbl.io/api/v2/friends', headers=headers)
+    #response = requests.get('https://xbl.io/api/v2/friends', headers=headers)
 
     # Use this to print header info if needed
     # print(response.headers)
 
-    print(response.status_code)
-    print(response.json())
-
-    # Parsing out from friends, who is a favorite and online (TEST)
+    gt = input('Type in your Xbox Gamertag: ')
+    # lookup by gamertag to get id
+    response = requests.get('https://xbl.io/api/v2/friends/search?gt={}'.format(gt), headers=headers)
     resp_data = response.json()
+    for data in resp_data['profileUsers']:
+        xuid = data['id']
+
+    # lookup by id to get data
+    response = requests.get('https://xbl.io/api/v2/friends?xuid={}'.format(xuid), headers=headers)
+    resp_data = response.json()
+
+    # Parsing out from friends
     for person in resp_data['people']:
-        if person['isFavorite'] and person['presenceState'] == 'Online':
-            print(person['displayName'])
+        ach_resp = requests.get('https://xbl.io/api/v2/achievements/player/{}'.format(person['xuid']), headers=headers)
+        achieve_data = ach_resp.json()
+
+        print('Player: ' + person['displayName'])
+        print('Id: ' + person['xuid'])
+        print('Gamerscore: ' + person['gamerScore'])
+        print('Presence: ' + person['presenceState'])
+        print()
+        # print games list for person
+        print('Games: ')
+        for achievement in achieve_data['titles']:
+            print(achievement['name'])
+        print()
+        print()
 
 
 if __name__ == '__main__':
