7e1c4dc4effd4fa6d45066c0c0510edc4cb6d8c4
Andrew Perrin <perrin2678@uwlax.edu>
2022-02-25 12:54:54 -0600

    added gamer tag error checking

--- GameLink.py
+++ GameLink.py
@@ -5,18 +5,27 @@ import json
 def connect():
     # Need this header to tie into my account info. If you want to test with your account, replace the
     # key from the OpenXBL Site
-    headers = {"X-Authorization": "wcckckkwgg4k0g4s8g4cgc0ggw08skskwwg"}
+    #ryan token wcckckkwgg4k0g4s8g4cgc0ggw08skskwwg
+    #andrew token k0cwwccokkogcgs0sgkgcgkcwskko0g8s8c
+
+    headers = {"X-Authorization": "k0cwwccokkogcgs0sgkgcgkcwskko0g8s8c"}
 
     # This is what we will be using to gather info from the api, what comes after the v2/ is where you put the
     # command that you want to get (friends, account, group, etc.) from this site: https://xbl.io/console
-    # response = requests.get('https://xbl.io/api/v2/friends', headers=headers)
-
+    #response = requests.get('https://xbl.io/api/v2/friends', headers=headers)
     gt = input('Type in your Xbox Gamertag: ')
     print()
 
     # lookup by gamertag to get id
     response = requests.get('https://xbl.io/api/v2/friends/search?gt={}'.format(gt), headers=headers)
     resp_data = response.json()
+
+    #Checks validity of gamertag and prompts the user again if it is invalid
+    while not( 'profileUsers' in resp_data.keys()):
+        print("!!Invalid Gamertag try again!!")
+        gt = input('Type in your Xbox Gamertag: ')
+        response = requests.get('https://xbl.io/api/v2/friends/search?gt={}'.format(gt), headers=headers)
+        resp_data = response.json()
 
     # Use this to print header info if needed
     # print(response.headers)
